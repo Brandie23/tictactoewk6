d20f216ce074d6d6fb429883ed7663a71e3b5708
Rubymoyes <moyesruby@gmail.com>
2018-11-23 14:15:47 +1300

    i fucking hate testing

---
+++ test/components.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import jest from 'jest'
+// import {shallow} from 'enzyme'
+
+test('<Game />', () => {
+const expected = '',
+    const wrapper = '',
+        expect().toBe()
+})
+
